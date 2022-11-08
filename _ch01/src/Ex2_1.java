972e6cfc914136ec2d929736e34cf07d8b9c8dd9
ParkSungHo <moonhj0105@naver.com>
2022-11-09 03:02:33 +0900

    day1_2

---
+++ _ch01/src/Ex2_1.java
@@ -0,0 +1,9 @@
+public class Ex2_1 {
+
+	public static void main(String[] args) {
+	  System.out.printf("Hello World");
+	  System.out.printf("Hello World");
+	  System.out.printf("Hello World");
+	}
+
+}
